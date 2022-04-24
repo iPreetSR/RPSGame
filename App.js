8447f77cd9adcbd65c5ac00ef96246e7d5635fa6
iPreetSR <ipreetsr@gmail.com>
2022-04-24 14:56:21 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import PlayScreen from './src/PlayScreen';
 
 export default function App() {
+
+  const [play, setPlay] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    play ? (
+      <PlayScreen/>
+    ) : (
+      <View style={styles.container}>
+        <Text style={styles.header}>Rock Paper Scissors</Text>
+        <Text style={{ fontSize:70 }}>✊ ✋ ✌️</Text>
+        <View style={styles.button}>
+          <Button onPress={() => setPlay(true)} title='Play'/>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    )
   );
 }
 
@@ -17,4 +30,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 28,
+    marginBottom: 30,
+  },
+  button: {
+    width: 240,
+    marginTop: 50,
+  }
 });
